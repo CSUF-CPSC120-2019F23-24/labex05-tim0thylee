1f07dbdd58aa694bc3464654371892f42289a99c
timothylee43 <timothylee43@cs104u-2f.acad.fullerton.edu>
2019-09-21 14:02:52 -0700

    complete lab 3

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,31 @@
 // Random Rectangle
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+int main ()
+{
+  // Create variables for dimensions, random numbers, and ouputting #.
+  const int MAX = 40;
+  const int HEIGHT = 3;
+  int length;
+  unsigned seed = time(0);
+  std::string hashtag_length;
+
+  // Create random seed number.
+  srand(seed);
+
+  // Set length to a randomm number that is at most 40.
+  length = rand() % MAX + 1;
+
+  // Create number of hashtags to output on length.
+  hashtag_length.assign(length, '#');
+
+  // Output reactangle with proper dimensions.
+  std::cout << hashtag_length << std::endl;
+  std::cout << hashtag_length << std::endl;
+  std::cout << hashtag_length << std::endl;
+
+  return 0;
+}
